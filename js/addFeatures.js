9faffa0bd2521174924097e2f7136d7bd2aa6024
ZheYD <327625449@qq.com>
2016-07-06 20:21:12 +0800

    add ForLiZhe projFiles

---
+++ js/addFeatures.js
@@ -0,0 +1,27 @@
+/**
+ * Created by kong on 2016/6/26.
+ */
+// require("./jquery-1.11.3.min.js");
+
+$('#page_2').click(function () {
+    setTimeout(function () {
+        self.location='pages/1314/1314.htm';
+    },2000);
+
+})
+
+$('#page_3').click(function () {
+    setTimeout(function () {
+        self.location='pages/luohua.html';
+    },2000);
+
+})
+
+$('#page_4').click(function () {
+    setTimeout(function () {
+        self.location='pages/Game/rabbit.html';
+    },2000);
+
+})
+
+
